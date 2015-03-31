d2e5b72897deee42be42ff9a8c49791650d8e16a
Dsaz <dsazztazz@gmail.com>
2015-03-31 22:21:37 +0300

    Community fixed

--- src/EspaceMembers/MainBundle/Controller/CommunityController.php
+++ src/EspaceMembers/MainBundle/Controller/CommunityController.php
@@ -2,28 +2,43 @@
 
 namespace EspaceMembers\MainBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use EspaceMembers\MainBundle\Controller\BaseController as Controller;
 
 class CommunityController extends Controller
 {
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, $page)
     {
         $em = $this->getDoctrine()->getManager();
 
-        return $this->render('EspaceMembersMainBundle:Community:communaute.html.twig', array(
-            'users'  => $em->getRepository('ApplicationSonataUserBundle:User')->findTeachers(),
-            'groups' => $em->getRepository('ApplicationSonataUserBundle:Group')->findAllNames(),
+        $pagerfanta = $this->getPagerfantaByArrayResults(
+            $em->getRepository('ApplicationSonataUserBundle:User')
+                ->findTeachersAndStudents()
+        );
+
+        $this->setCurrentPageOr404($pagerfanta, $page);
+
+        return $this->render('EspaceMembersMainBundle:Community:community.html.twig', array(
+            'paginator' => $pagerfanta,
+            'users'     => $pagerfanta->getCurrentPageResults(),
+            'groups'    => $em->getRepository('ApplicationSonataUserBundle:Group')->findAllNames(),
         ));
     }
 
-    public function filterByGroupAction(Request $request)
+    public function filterByGroupAction(Request $request, $page)
     {
         $em = $this->getDoctrine()->getManager();
 
-        return $this->render('EspaceMembersMainBundle:Community:communaute.html.twig', array(
-            'users'  => $em->getRepository('ApplicationSonataUserBundle:User')
-                ->findTeachersByGroup($request->get('group_name')),
+        $pagerfanta = $this->getPagerfantaByArrayResults(
+            $em->getRepository('ApplicationSonataUserBundle:User')
+                ->findTeacherAndStudentsByGroup($request->get('group_name'))
+        );
+
+        $this->setCurrentPageOr404($pagerfanta, $page);
+
+        return $this->render('EspaceMembersMainBundle:Community:community.html.twig', array(
+            'paginator' => $pagerfanta,
+            'users'     => $pagerfanta->getCurrentPageResults(),
             'groups' => $em->getRepository('ApplicationSonataUserBundle:Group')->findAllNames(),
         ));
     }
